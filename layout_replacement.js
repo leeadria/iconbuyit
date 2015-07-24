31c944c9bab7983680cc97ad02e9aaeb139144f2
Hirakawa <kentoh@a45e60cd49dd.ant.amazon.com>
2015-07-24 10:43:36 +0900

    minor change

--- layout_replacement.js
+++ layout_replacement.js
@@ -7,17 +7,7 @@
 // @grant       none
 // ==/UserScript==
 
-var load = window.setTimeout(function() {
+function change_layout() {
   var prodDetails = document.getElementById('prodDetails');
   prodDetails.parentNode.removeChild(prodDetails);
-  
-  var child = document.getElementsByClassName('small')[0]
-  child.parentNode.insertBefore(prodDetails,child);
-  
-  var stl = document.createElement('style');
-  stl.type = 'text/css';
-  var css = document.createTextNode('ul.icons { list-style-type: none; margin: 0px 5px auto 0px; padding: 0 0 30px 0;} ul.icons li { display: inline; } ul.icons li a { color: #42454a; border: 1px solid #c9c3ba; padding: 10px; text-decoration: none; width: 115px; height: 40px; }');
-  stl.appendChild(css);
-  var head = document.getElementsByTagName('head');
-  head[0].appendChild(stl);
 });
